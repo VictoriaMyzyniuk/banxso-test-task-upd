3a095fb7279c791f6a2ba539f3b377c02e0a919f
VictoriaMyzyniuk <99035329+VictoriaMyzyniuk@users.noreply.github.com>
2024-07-09 21:21:36 +0000

    Deploying to gh-pages from @ VictoriaMyzyniuk/banxso-test-task-upd@678097f8c11055e729cb18df310e62d91a0fd5a0 🚀

---
+++ index.0bbfa27b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".slick-next-custom"),o=document.querySelector(".slick-prev-custom");$(document).ready((function(){$(".multiple-items").slick({dots:!0,infinite:!0,speed:500,slidesToShow:3,slidesToScroll:1,prevArrow:o,nextArrow:e,centerMode:!0})}));
+//# sourceMappingURL=index.0bbfa27b.js.map
